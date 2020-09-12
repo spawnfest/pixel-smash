fab3ece8ff07a70b083c1b4b295b61cb6e6e9634
Quinn Wilton <wilton@synopsys.com>
2020-09-11 21:00:45 -0700

    Create PixelSmash.Battles to provide a public interface to battles

--- lib/pixel_smash/battles.ex
+++ lib/pixel_smash/battles.ex
@@ -1,3 +1,22 @@
 defmodule PixelSmash.Battles do
-  alias PixelSmash.Battles
+  alias PixelSmash.Battles.{
+    Battle,
+    Fighter
+  }
+
+  def create_battle(%Fighter{} = left, %Fighter{} = right) do
+    Battle.InProgress.new({left, right})
+  end
+
+  def tick_battle(%Battle.InProgress{} = battle) do
+    Battle.simulate_tick(battle)
+  end
+
+  def narrate_battle(%Battle.InProgress{} = battle) do
+    Battle.narrate(battle)
+  end
+
+  def narrate_battle(%Battle.Finished{} = battle) do
+    Battle.narrate(battle)
+  end
 end
