776012a60a34a4ae60f518b6a5606a1cf7cae8be
Ivan Rublev <ivan@ivanrublev.me>
2020-09-12 16:54:06 +0200

    Map sprite to attribute

--- lib/pixel_smash/gladiators/sprite_mapper.ex
+++ lib/pixel_smash/gladiators/sprite_mapper.ex
@@ -4,11 +4,19 @@ defmodule PixelSmash.Gladiators.SpriteMapper do
 
   ## Examples
 
-      iex> alias PixelSmash.Gladiators.SpriteMapper
-      ...> alias PixelSmash.Sprites.Sprite
-      iex> attributes = [exhaustion: 50, health: 100, strength: 10, speed: 60, magic: 80]
+  It can map list of attributes to a Sprite
+
+      iex> alias PixelSmash.{Gladiators.SpriteMapper, Sprites.Sprite}
+      ...> attributes = [exhaustion: 50, health: 100, strength: 10, speed: 60, magic: 80]
       ...> %Sprite{data: [row1 | _]} = SpriteMapper.sprite(attributes)
       ...> [{:black, 50}, {:green, 100}, {:red, 10}, {:purple, 60}, {:blue, 80} | _] = row1
+
+  And map a Sprite to list of attributes
+
+      iex> alias PixelSmash.{Gladiators.SpriteMapper, Sprites}
+      ...> sprite = Sprites.new_sprite([{:black, 40}, {:black, 50}, {:green, 100}, {:red, 10}, {:purple, 60}, {:blue, 80}, {:gray, 100}])
+      ...> attributes = SpriteMapper.attributes(sprite)
+      ...> [exhaustion: 50, health: 100, strength: 10, speed: 60, magic: 80] = attributes
   """
 
   alias PixelSmash.Sprites
@@ -38,7 +46,24 @@ defmodule PixelSmash.Gladiators.SpriteMapper do
     Sprites.new_pixel(base_color, value)
   end
 
-  def attributes(%Sprite{map: map} = sprite) do
+  def attributes(%Sprite{data: data}) do
+    base_colors = Enum.map(@attribute_base_color, &elem(&1, 1))
 
+    data
+    |> List.flatten()
+    |> Enum.filter(fn {color, _tint} -> color in base_colors end)
+    |> Enum.reduce([], &put_attribute(&2, &1))
+    |> Enum.reverse()
+  end
+
+  defp put_attribute(acc, {_base_color, tint} = color) do
+    Keyword.put(acc, find_attribute(color), tint)
+  end
+
+  def find_attribute({base_color, _tint}) do
+    Enum.find_value(@attribute_base_color, fn
+      {attribute, ^base_color} -> attribute
+      _ -> nil
+    end)
   end
 end
