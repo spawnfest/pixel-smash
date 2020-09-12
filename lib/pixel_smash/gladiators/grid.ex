1c72189c69db9daf4ad176be70fbfef944e981d9
chouzar <wychouza@gmail.com>
2020-09-12 11:26:42 -0700

    More specs and notes for the Grid soon to be Sprite module

--- lib/pixel_smash/gladiators/grid.ex
+++ lib/pixel_smash/gladiators/grid.ex
@@ -20,23 +20,38 @@ defmodule PixelSmash.Gladiators.Grid do
 
   No matter the generation fuction, grids are always mirrored in the y-vertical axis.
   """
+  @spec new(x :: pos_integer(), y :: pos_integer(), generator_fn :: (() -> term())) :: t()
   def new(size_x, size_y, generator_fn \\ &nils/0) do
     %__MODULE__{x: size_x, y: size_y}
     |> generate(generator_fn)
   end
 
-  # @spec apply_mask(grid :: t(), mask :: t()) :: t()
-  def apply_mask(%__MODULE__{} = grid, %__MODULE__{} = mask, apply_fn \\ &merge/3) do
+  @doc """
+  Applies a "mask" over the first grid argument. By default the original grid will be overritten
+  by the mask but it is possible to pass a merge function to decide between values.
+
+  If the mask "overflows" the original grid it will be cut from the end result.
+  """
+  @spec apply_mask(
+          grid :: t(),
+          mask :: t(),
+          merge_fn :: (key :: any(), value1 :: any(), value2 :: any() -> any())
+        ) :: t()
+  def apply_mask(%__MODULE__{} = grid, %__MODULE__{} = mask, merge_fn \\ &merge/3) do
     mask_map =
       mask.map
       |> Enum.reject(fn {{x, y}, _value} -> x > grid.x or y > grid.y end)
       |> Enum.reject(fn {{x, y}, _value} -> x < 1 or y < 1 end)
       |> Enum.into(%{})
 
-    map = Map.merge(grid.map, mask_map, apply_fn)
+    map = Map.merge(grid.map, mask_map, merge_fn)
     Map.put(grid, :map, map)
   end
 
+  @doc """
+  Modifies the original position of any grid by applying a sum to the original coordinates.
+  The sum here is considered boundless positive or negative values.
+  """
   @spec position(grid :: t(), at :: coor()) :: t()
   def position(%__MODULE__{} = grid, at \\ {0, 0}) do
     map =
@@ -49,18 +64,22 @@ defmodule PixelSmash.Gladiators.Grid do
   end
 
   defp generate(%__MODULE__{} = grid, generator_fn) do
+    # Generates a list of lists according to the generator function
     data =
       for _ <- 1..grid.y do
         generate_row(grid.x, generator_fn)
       end
 
+    # Creates a list of coordinates of the specified grid size
     coordinates =
       for y <- 1..grid.y, x <- 1..grid.x do
         {x, y}
       end
 
+    # Finally we flatten the original list of lists
     elements = Enum.flat_map(data, fn x -> x end)
 
+    # And merge it with coordinates to create our "map"
     map = Enum.zip(coordinates, elements) |> Enum.into(%{})
     Map.put(grid, :map, map)
   end
@@ -91,8 +110,10 @@ defmodule PixelSmash.Gladiators.Grid do
     {xa + xb, ya + yb}
   end
 
+  # Default anonymous function for the `apply_mask` function
   defp merge(_key, _value1, value2), do: value2
 
+  # Default anonymous function for the `new` function
   defp nils(), do: nil
 
   @spec even?(value :: integer()) :: boolean()
@@ -100,6 +121,7 @@ defmodule PixelSmash.Gladiators.Grid do
     rem(value, 2) == 0
   end
 
+  # Print helper for debugger, should use the stdout Protocol
   def ins(%__MODULE__{} = grid) do
     for y <- 1..grid.y do
       row =
