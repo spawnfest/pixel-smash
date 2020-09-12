d304fe5675ac4c3755ea0c600ac3352aab8fa94c
Quinn Wilton <wilton@synopsys.com>
2020-09-11 23:18:07 -0700

    Remove default LiveView code

--- lib/pixel_smash_web/live/page_live.ex
+++ lib/pixel_smash_web/live/page_live.ex
@@ -6,47 +6,7 @@ defmodule PixelSmashWeb.PageLive do
     socket =
       socket
       |> assign_defaults(params, session)
-      |> assign(query: "", results: %{})
 
     {:ok, socket}
-  end
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case socket.assigns.current_user do
-      nil ->
-        raise "NOT LOGGED IN"
-
-      user ->
-        IO.inspect(user)
-
-        case search(query) do
-          %{^query => vsn} ->
-            {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-          _ ->
-            {:noreply,
-             socket
-             |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-             |> assign(results: %{}, query: query)}
-        end
-    end
-  end
-
-  defp search(query) do
-    if not PixelSmashWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
