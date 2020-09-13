2e3aa8c825a711c14179388b3d2be55d97d38680
Ivan Rublev <ivan@ivanrublev.me>
2020-09-13 19:40:53 +0200

    variate narrations

--- lib/pixel_smash/battles/log.ex
+++ lib/pixel_smash/battles/log.ex
@@ -5,28 +5,61 @@ defmodule PixelSmash.Battles.Log do
 
   defdata do
     actions :: [Battles.Actions.t()] \\ []
+    narrations :: [String.t()] \\ []
+  end
+
+  def narrate(%Battles.Log{narrations: narrations}) do
+    narrations
   end
 
   def append(%Battles.Log{} = log, action) do
-    %Battles.Log{log | actions: log.actions ++ [action]}
-  end
-
-  def narrate(%Battles.Log{} = log) do
-    Enum.map(log.actions, &narrate_action/1)
+    %Battles.Log{log
+    | actions: log.actions ++ [action],
+      narrations: log.narrations ++ [narrate_action(action)]}
   end
 
   defp narrate_action(%Battles.Action.Attack{} = action) do
-    [
-      action.fighter.name,
-      " leaps into action and hits ",
-      action.target.name,
-      " for ",
-      Integer.to_string(action.damage),
-      " damage!"
-    ]
+    attack_narration(Enum.random(1..3), action)
   end
 
   defp narrate_action(%Battles.Action.Cast{} = action) do
+    cast_narration(Enum.random(1..3), action)
+  end
+
+  defp attack_narration(1, action) do
+    [
+    action.fighter.name,
+    " leaps into action and hits ",
+    action.target.name,
+    " for ",
+    Integer.to_string(action.damage),
+    " damage!"
+    ]
+  end
+
+  defp attack_narration(2, action) do
+    [
+    action.fighter.name,
+    " attacks and makes ",
+    Integer.to_string(action.damage),
+    " damage to ",
+    action.target.name,
+    "."
+    ]
+  end
+
+  defp attack_narration(3, action) do
+    [
+      action.fighter.name,
+      " tirelessly strikes ",
+      action.target.name,
+      " with ",
+      Integer.to_string(action.damage),
+      " damage."
+    ]
+  end
+
+  defp cast_narration(1, action) do
     [
       action.fighter.name,
       " conjures up ",
@@ -36,6 +69,32 @@ defmodule PixelSmash.Battles.Log do
       " for ",
       Integer.to_string(action.damage),
       " damage!"
+    ]
+  end
+
+  defp cast_narration(2, action) do
+    [
+      action.fighter.name,
+      " calls the force of ",
+      action.spell_name,
+      " and makes ",
+      Integer.to_string(action.damage),
+      " damage to ",
+      action.target.name,
+      ".",
+    ]
+  end
+
+  defp cast_narration(3, action) do
+    [
+      action.fighter.name,
+      " ventures a spell of the ",
+      action.spell_name,
+      " on ",
+      action.target.name,
+      " with ",
+      Integer.to_string(action.damage),
+      " damage."
     ]
   end
 end
