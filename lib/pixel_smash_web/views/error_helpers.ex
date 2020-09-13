572be641bd857f6a3a4d7fed4a9bf45ee5409daf
Quinn Wilton <wilton@synopsys.com>
2020-09-12 23:25:15 -0700

    Implement settings / login / registration forms for dark theme

--- lib/pixel_smash_web/views/error_helpers.ex
+++ lib/pixel_smash_web/views/error_helpers.ex
@@ -11,7 +11,7 @@ defmodule PixelSmashWeb.ErrorHelpers do
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+        class: "invalid-feedback block",
         phx_feedback_for: input_id(form, field)
       )
     end)
