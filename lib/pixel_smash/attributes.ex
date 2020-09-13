0c8dc4fc71f47ebb0a851659610ff72115e705d5
chouzar <wychouza@gmail.com>
2020-09-13 01:55:24 -0700

    Modified attribute names

--- lib/pixel_smash/attributes.ex
+++ lib/pixel_smash/attributes.ex
@@ -1,10 +1,10 @@
 defmodule PixelSmash.Attributes do
-  @perseverance [:stamina, :defense]
+  @perseverance [:vitality, :defense]
   @combat [:strength, :casting]
   @special [:speed, :secret]
 
   def all(), do: Enum.concat([@perseverance, @combat, @special])
-  def perseverance(), do: Enum.random([:stamina, :defense])
+  def perseverance(), do: Enum.random([:vitality, :defense])
   def combat(), do: Enum.random([:strength, :casting])
   def special(), do: Enum.random([:speed, :secret])
   def noop(), do: :no_operation
@@ -13,12 +13,12 @@ defmodule PixelSmash.Attributes do
     case attribute do
       :empty -> :transparent
       :no_operation -> :gray
-      :stamina -> :green
-      :casting -> :blue
-      :speed -> :yellow
+      :vitality -> :green
+      :defense -> :blue
       :strength -> :red
-      :vitality -> :purple
-      secret -> :pink
+      :casting -> :purple
+      :speed -> :yellow
+      :secret -> :pink
     end
   end
 end
