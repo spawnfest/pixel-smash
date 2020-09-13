424945ed4361c58d43a3fce64e2ddd449cd014e9
Ivan Rublev <ivan@ivanrublev.me>
2020-09-13 23:50:30 +0200

    map sprite to cast actions

--- lib/pixel_smash/battles/action_deck.ex
+++ lib/pixel_smash/battles/action_deck.ex
@@ -1,18 +1,17 @@
 defmodule PixelSmash.Battles.ActionDeck do
   alias PixelSmash.{
     Battles.Action,
-    Battles.Fighter,
     Sprites
   }
 
   # counts of colors should be >= 0
-  @attack_multiplier [
+  @colors_attack_multiplier [
     {%{red: 12, pink: 5}, 10},
     {%{dark_red: 7, dark_green: 3}, 5},
     {%{red: 3}, 1}
   ]
 
-  @spell_multiplier [
+  @colors_cast_multiplier [
     {%{purple: 12, dark_pink: 5}, 10},
     {%{dark_purple: 12, vitality: 5}, 2},
     {%{purple: 12}, 1}
@@ -24,20 +23,30 @@ defmodule PixelSmash.Battles.ActionDeck do
   end
 
   def actions(stats, fighter, opponent) do
-    {_, attack_actions} = Enum.reduce(@attack_multiplier, {stats, []}, fn {color_count, multiplier}, {stats, actions} ->
-      {stats, reductions_count} = reduce_stats(stats, color_count)
-      attacks = List.duplicate(attack_action(fighter, opponent, multiplier), reductions_count)
-      {stats, actions ++ attacks}
-    end)
-  end
+    {_, attack_actions} =
+      Enum.reduce(@colors_attack_multiplier, {stats, []}, fn {color_count, multiplier},
+                                                             {stats, actions} ->
+        {stats, reductions_count} = reduce_stats(stats, color_count)
+        attacks = List.duplicate(attack_action(fighter, opponent, multiplier), reductions_count)
+        {stats, actions ++ attacks}
+      end)
 
-  defp expand_to_list(map) do
-    Enum.flat_map(map, fn {key, value} -> List.duplicate(key, value) end)
+    {_, cast_actions} =
+      Enum.reduce(@colors_cast_multiplier, {stats, []}, fn {color_count, multiplier},
+                                                           {stats, actions} ->
+        {stats, reductions_count} = reduce_stats(stats, color_count)
+        attacks = List.duplicate(cast_action(fighter, opponent, multiplier), reductions_count)
+        {stats, actions ++ attacks}
+      end)
+
+    attack_actions ++ cast_actions
   end
 
   defp reduce_stats(stats, color_count, reductions_count \\ 0) do
-    reduced_stats = Enum.reduce_while(color_count, stats, fn {color, count}, stats ->
+    reduced_stats =
+      Enum.reduce_while(color_count, stats, fn {color, count}, stats ->
         stats_count = Map.get(stats, color, -1)
+
         if stats_count >= count do
           {:cont, Map.put(stats, color, stats_count - count)}
         else
@@ -57,9 +66,17 @@ defmodule PixelSmash.Battles.ActionDeck do
       fighter: fighter,
       target: opponent,
       damage: fighter.strength * multiplier
-     }
+    }
   end
 
+  defp cast_action(fighter, opponent, multiplier) do
+    %Action.Cast{
+      fighter: fighter,
+      target: opponent,
+      damage: fighter.magic * multiplier,
+      spell_name: Enum.random(fighter.spells)
+    }
+  end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
