f780bbcd6fa29bc5c4790d1e127891a078bc221e
Quinn Wilton <wilton@synopsys.com>
2020-09-12 23:28:45 -0700

    Fix controller tests

--- test/pixel_smash_web/controllers/user_registration_controller_test.exs
+++ test/pixel_smash_web/controllers/user_registration_controller_test.exs
@@ -7,9 +7,7 @@ defmodule PixelSmashWeb.UserRegistrationControllerTest do
     test "renders registration page", %{conn: conn} do
       conn = get(conn, Routes.user_registration_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Register</h1>"
-      assert response =~ "Log in</a>"
-      assert response =~ "Register</a>"
+      assert response =~ "Register"
     end
 
     test "redirects if already logged in", %{conn: conn} do
@@ -35,8 +33,6 @@ defmodule PixelSmashWeb.UserRegistrationControllerTest do
       conn = get(conn, "/")
       response = html_response(conn, 200)
       assert response =~ email
-      assert response =~ "Settings</a>"
-      assert response =~ "Log out</a>"
     end
 
     test "render errors for invalid data", %{conn: conn} do
@@ -46,7 +42,7 @@ defmodule PixelSmashWeb.UserRegistrationControllerTest do
         })
 
       response = html_response(conn, 200)
-      assert response =~ "<h1>Register</h1>"
+      assert response =~ "Register"
       assert response =~ "must have the @ sign and no spaces"
       assert response =~ "should be at least 12 character"
     end
--- test/pixel_smash_web/controllers/user_session_controller_test.exs
+++ test/pixel_smash_web/controllers/user_session_controller_test.exs
@@ -11,9 +11,7 @@ defmodule PixelSmashWeb.UserSessionControllerTest do
     test "renders log in page", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
-      assert response =~ "Log in</a>"
-      assert response =~ "Register</a>"
+      assert response =~ "Log In"
     end
 
     test "redirects if already logged in", %{conn: conn, user: user} do
@@ -36,8 +34,6 @@ defmodule PixelSmashWeb.UserSessionControllerTest do
       conn = get(conn, "/")
       response = html_response(conn, 200)
       assert response =~ user.email
-      assert response =~ "Settings</a>"
-      assert response =~ "Log out</a>"
     end
 
     test "logs the user in with remember me", %{conn: conn, user: user} do
@@ -61,7 +57,7 @@ defmodule PixelSmashWeb.UserSessionControllerTest do
         })
 
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
+      assert response =~ "Log In"
       assert response =~ "Invalid email or password"
     end
   end
